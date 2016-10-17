ca5e38e0a4e71bcd96c6eb634ff66b0ac4145d8a
Harshvardhan <hruchand@uncc.edu>
2016-10-17 02:21:22 -0400

    commit 10/17/2016

---
+++ app/src/main/java/edu/sprakas1uncc/iotproject/Home.java
@@ -0,0 +1,16 @@
+package edu.sprakas1uncc.iotproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by siddhartha on 10/15/2016.
+ */
+
+public class Home extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+    }
+}
