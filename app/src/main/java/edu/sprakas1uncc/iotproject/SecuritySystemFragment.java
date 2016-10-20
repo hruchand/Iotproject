1bea8cd904d69bc6d065ab540a80c45718732ccc
Harshvardhan Ruchandani <hruchand@uncc.edu>
2016-10-20 11:50:38 -0400

    working Nav bar, sql, successfull login & registration + layout(home page)+lights(java)+Locks(java)+radio button(java)

--- app/src/main/java/edu/sprakas1uncc/iotproject/SecuritySystemFragment.java
+++ app/src/main/java/edu/sprakas1uncc/iotproject/SecuritySystemFragment.java
@@ -3,9 +3,13 @@ package edu.sprakas1uncc.iotproject;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 
 /**
@@ -23,7 +27,40 @@ public class SecuritySystemFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_security_system, container, false);
-    }
+        View v = inflater.inflate(R.layout.fragment_security_system, container, false);
+        RadioGroup radioGroup = (RadioGroup) v.findViewById(R.id.radiogroup);
+        radioGroup.clearCheck();
 
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton rb = (RadioButton) group.findViewById(checkedId);
+                if (null != rb && checkedId > -1) {
+                    Log.d("radio button", "not pressed");
+                                    }
+                if  (checkedId == R.id.armedaway){
+                    Log.d("radio", "armedaway");
+                }
+
+            }
+        });
+        return v;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
