77d11b70e0ce9d3872c51839c2f3c6fcea0c6659
Harshvardhan Ruchandani <hruchand@uncc.edu>
2016-10-27 04:30:55 -0400

    working Nav bar, sql, successfull login & registration + layout(home page)+lights(java)+Locks(java)+radio button(java)+thermostat(xml)+thermostat(java)+threading+energy(java)+energy(xml)+Final_light_layout(xml)+Final commit as on 10/24/2016 12:25pm+ query data for thermostat from database.+query data for Energy thermostat from database.+ wow!! updated controltempmain+both way func light & therm+ dual func sec sys

--- app/src/main/java/edu/sprakas1uncc/iotproject/SecuritySystemFragment.java
+++ app/src/main/java/edu/sprakas1uncc/iotproject/SecuritySystemFragment.java
@@ -9,7 +9,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 /**
@@ -17,11 +23,11 @@ import android.widget.Toast;
  */
 public class SecuritySystemFragment extends Fragment {
 
-
+public static String sys_status = "off";
     public SecuritySystemFragment() {
         // Required empty public constructor
     }
-
+int trmp = fetchData();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -29,7 +35,9 @@ public class SecuritySystemFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_security_system, container, false);
         RadioGroup radioGroup = (RadioGroup) v.findViewById(R.id.radiogroup);
+        final TextView textView = (TextView) v.findViewById(R.id.sys_status);
         radioGroup.clearCheck();
+        textView.setText(sys_status);
 
 
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
@@ -38,21 +46,120 @@ public class SecuritySystemFragment extends Fragment {
                 RadioButton rb = (RadioButton) group.findViewById(checkedId);
                 if (null != rb && checkedId > -1) {
                     Log.d("radio button", "not pressed");
+                    String sys_status = "off";
                                     }
                 if  (checkedId == R.id.armedaway){
                     Log.d("radio", "armedaway");
+                    sys_status = "armed_away";
+                    textView.setText(sys_status);
+                    setLightData(sys_status);
                 }
                 if (checkedId == R.id.armedstay){
                     Log.d("radio","armedstay");
+                    sys_status = "armed_stay";
+                    textView.setText(sys_status);
+                    setLightData(sys_status);
                 }
                 if (checkedId == R.id.disarmed){
                     Log.d("radio","disarmed");
+                    sys_status = "disarmed";
+                    textView.setText(sys_status);
+                    setLightData(sys_status);
                 }
 
             }
         });
         return v;
     }
+
+    public int fetchData() {
+        try {
+
+            int temp_curr;
+            String cId = "1";
+            String url = "http://192.168.1.3/fetchSecurity.php";
+            URL urlObj = new URL(url);
+            String result = "";
+            String data = "cId=" + java.net.URLEncoder.encode(cId, "UTF-8");
+            //1
+            HttpURLConnection conn = (HttpURLConnection) urlObj.openConnection();
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+            conn.setUseCaches(false);
+            conn.setRequestMethod("POST");
+            //2
+            DataOutputStream dataOut = new DataOutputStream(conn.getOutputStream());
+            dataOut.writeBytes(data);
+            //3
+            dataOut.flush();
+            dataOut.close();
+            DataInputStream in = new DataInputStream(conn.getInputStream());
+
+            String g;
+            while ((g = in.readLine()) != null) {
+
+                result += g;
+
+            }
+            Log.d("fetchdata", "inside fetch data");
+
+            in.close();
+            Log.d("fetchdata", "value" + result);
+            sys_status = result;
+
+
+        } catch (Exception e) {
+
+        }
+
+        return 0;
+
+    }
+
+    public void setLightData(String x) {
+
+        try {
+            int temp_curr;
+            String cId = "1";
+            String url = "http://192.168.1.3/setSecurity.php";
+            URL urlObj = new URL(url);
+            String result = "";
+            String data = "cId=" + java.net.URLEncoder.encode(cId, "UTF-8");
+            String data1 = " " + java.net.URLEncoder.encode(x, "UTF-8");
+            //1
+            HttpURLConnection conn = (HttpURLConnection) urlObj.openConnection();
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+            conn.setUseCaches(false);
+            conn.setRequestMethod("POST");
+            //2
+            DataOutputStream dataOut = new DataOutputStream(conn.getOutputStream());
+            dataOut.writeBytes(data);
+            dataOut.writeBytes(data1);
+            //3
+            dataOut.flush();
+            dataOut.close();
+            DataInputStream in = new DataInputStream(conn.getInputStream());
+
+            String g;
+            while ((g = in.readLine()) != null) {
+
+                result += g;
+
+            }
+            Log.d("fetchdata", "inside fetch data");
+
+            in.close();
+            //Log.d("fetchdata", "value" + result);
+            //String[] numbersArray = result.split(" ");
+            // current_temp_upstairs = Integer.parseInt(numbersArray[0]);
+            // current_temp_main = Integer.parseInt(numbersArray[1]);
+        } catch (Exception e) {
+
+        }
+    }
+
+
 }
 
 
